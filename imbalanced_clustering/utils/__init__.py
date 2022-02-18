48f8ab8fda7f1adc284d8dec82a8929c1c037594
hsmaan <hmaan@uoguelph.ca>
2022-02-18 17:54:07 -0500

    fixed cython script import issue with pyximport

--- imbalanced_clustering/utils/__init__.py
+++ imbalanced_clustering/utils/__init__.py
@@ -1,8 +1,13 @@
+import pyximport
+import numpy
+pyximport.install(
+    setup_args={
+        "include_dirs":numpy.get_include()
+    },
+    reload_support=True
+)
+from ._emi_cython import expected_mutual_information
 from .contingency import pair_confusion_matrix, contingency_matrix
 from .checks import check_clusterings
-from .mi import mutual_info_score, entropy, expected_mutual_information
+from .mi import mutual_info_score, entropy
 from .avg import generalized_average
-from ._emi_cython import expected_mutual_information 
-
-check_clusterings, contingency_matrix, entropy, mutual_info_score, \
-    expected_mutual_information, generalized_average
