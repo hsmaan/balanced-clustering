4993b9ab218febf3762ae905862289bc26bf721c
Adam Gayoso <adamgayoso@users.noreply.github.com>
2022-11-16 12:51:38 -0800

    Update __init__.py

--- balanced_clustering/utils/__init__.py
+++ balanced_clustering/utils/__init__.py
@@ -2,7 +2,7 @@ import pyximport
 import numpy
 
 pyximport.install(setup_args={"include_dirs": numpy.get_include()}, reload_support=True)
-from ._emi_cython import expected_mutual_information
+from ._emi import expected_mutual_information
 from .contingency import pair_confusion_matrix, contingency_matrix
 from .checks import check_clusterings
 from .mi import mutual_info_score, entropy
